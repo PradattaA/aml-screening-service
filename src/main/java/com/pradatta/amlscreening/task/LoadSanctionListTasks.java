531e17ebd0a0d3662c7ad1736c7aaa4e8fe46c38
Pradatta <pradatta.adhikary@icloud.com>
2023-06-10 23:47:06 +0100

    Aml Screening Service -- Cron job to run every day to load Sanctioned List

--- src/main/java/com/pradatta/amlscreening/task/LoadSanctionListTasks.java
+++ src/main/java/com/pradatta/amlscreening/task/LoadSanctionListTasks.java
@@ -1,11 +1,14 @@
 package com.pradatta.amlscreening.task;
 
 
+import com.pradatta.amlscreening.service.SanctionEntityLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.net.MalformedURLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -14,6 +17,9 @@ import java.util.Date;
 public class LoadSanctionListTasks {
     private static final Logger log = LoggerFactory.getLogger(LoadSanctionListTasks.class);
 
+    @Autowired
+    private SanctionEntityLoader sanctionEntityLoader;
+
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
     /**
@@ -21,8 +27,13 @@ public class LoadSanctionListTasks {
      * For now it's just set to run every minute and log.
      * Will implement loading logic by downloading it from internet and then load it in to database
      */
-    @Scheduled( cron = "0 */1 * ? * *")
+    @Scheduled(cron = "0 0 * * * ?")
     public void reportCurrentTime() {
         log.info("Loading the sanction lists at {}", dateFormat.format(new Date()));
+        try {
+            sanctionEntityLoader.loadSanctionFile();
+        } catch (MalformedURLException e) {
+            log.error(e.getMessage(), e);
+        }
     }
 }
